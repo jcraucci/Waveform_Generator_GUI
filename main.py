2acdd4c738bb675022a916bb3551ecb634bbda34
imkewl1 <jcraucci@neiu.edu>
2025-06-19 11:10:19 -0500

    Created functions for each tab and started waveform tab function

--- main.py
+++ main.py
@@ -20,13 +20,36 @@ class Display:
         notebook = ttk.Notebook(self.root)
         notebook.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-        general_frame = ttk.Frame(notebook)
         self.waveform_frame = ttk.Frame(notebook)
-        export_frame = ttk.Frame(notebook)
+        self.fourier_frame = ttk.Frame(notebook)
+        self.export_frame = ttk.Frame(notebook)
 
-        notebook.add(general_frame, text='')
-        notebook.add(self.waveform_frame, text='')
-        notebook.add(export_frame, text='')
+        notebook.add(self.waveform_frame, text='Waveform Generator')
+        notebook.add(self.fourier_frame, text='Fourier Series')
+        notebook.add(self.export_frame, text='Export Options')
+
+        self.setup_waveform_tab()
+        #self.setup_fourier_tab()
+        #self.setup_export_tab()
+    def setup_waveform_tab(self):
+        self.control_frame = ttk.Frame(self.waveform_frame)
+        self.control_frame.grid(row=0, column=0, columnspan=6, sticky='w')
+
+        ttk.Label(self.control_frame, text="Waveform Type").grid(row=0, column=0, sticky='w')
+        self.waveform_type_combobox = ttk.Combobox(self.control_frame, values=["Sine", "Square", "Sawtooth", "Triangle"], state="readonly")
+        self.waveform_type_combobox.grid(row=1, column=0)
+        self.waveform_type_combobox.current(0)  # optional: sets default to "Sine"
+        self.waveform_type_combobox.bind("<<ComboboxSelected>>", self.generate_waveform)
+
+    #def setup_fourier_tab(self):
+    
+    #def setup_export_tab(self):
+
+
+    
+    def generate_waveform(self):
+        wave_type = self.waveform_type_combobox.get()
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
